a6711beab6d3838ef2dab884d2dc1fdd300fcf1c
pipe <pipelonq76@gmail.com>
2021-08-02 10:23:36 -0500

    restauracion db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,29 +1,18 @@
 const mysql=require('mysql');
-const conexion_bd = {
+const connection = mysql.createConnection({
 	host : "us-cdbr-east-04.cleardb.com",
 	user : "b8ecdce97e031d",
-	database : "a9e4bfcd",
-	password : "heroku_061e30f49b88c2b",
-};
-
-function handleDisconnect(conexion_bd){
-	connection = mysql.createPool(conexion_bd);
+	database : "heroku_061e30f49b88c2b",
+	password : "a9e4bfcd",
+});
 
 
-	connection.getConnection(function(err){
-		if(err){
-			console.log('error en la conexion de bases de datos:', err);
-			setTimeout(handleDisconnect, 2000);
-		}
-	});
-
-	connection.on('err', function(err){
-		console.log('db err', err);
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			handleDisconnect();
-		} else {
-			throw err;
-		}
-	});
+connection.connect((error)=>{
+	if(error){
+		console.log("este error se presenta " + error)
+		return;
 	}
-handleDisconnect(conexion_bd);
+	console.log("conectados exitosamente a la base de datos")
+})
+
+module.exports=connection;
