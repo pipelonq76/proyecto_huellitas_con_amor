19cab41fbd5ead95d6d9cccd5eba6bcd34adbccf
pipe <pipelonq76@gmail.com>
2021-08-01 19:20:18 -0500

    dsdsdsdsjjh

--- src/config/db.js
+++ src/config/db.js
@@ -10,19 +10,19 @@ function handleDisconnect(conexion_bd){
 	connection = mysql.createPool(conexion_bd);
 
 
-	connection.getConnection(function(error){
-		if(error){
-			console.log('error en la conexion de bases de datos:', error);
+	connection.getConnection(function(err){
+		if(err){
+			console.log('error en la conexion de bases de datos:', err);
 			setTimeout(handleDisconnect, 2000);
 		}
 	});
 
-	connection.on('error', function(error){
-		console.log('db error', error);
-		if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+	connection.on('err', function(err){
+		console.log('db err', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
 			handleDisconnect();
 		} else {
-			throw error;
+			throw err;
 		}
 	});
 	}
